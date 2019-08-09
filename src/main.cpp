b10f668797a749aba7d046ef0b3f482cc5152235
Guy Wilson <wilson.guy@gmail.com>
2019-08-09 18:40:27 +0100

    Turn off debuf logging

--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,8 @@
 #include "views.h"
 #include "logger.h"
 
-#define LOG_LEVEL			LOG_LEVEL_INFO | LOG_LEVEL_ERROR | LOG_LEVEL_FATAL | LOG_LEVEL_DEBUG
-#define SERIAL_EMULATION
+#define LOG_LEVEL			LOG_LEVEL_INFO | LOG_LEVEL_ERROR | LOG_LEVEL_FATAL //| LOG_LEVEL_DEBUG
+//#define SERIAL_EMULATION
 
 using namespace std;
 
