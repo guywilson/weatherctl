45a5dc396c8aa70021e3e619802155d78e39f53f
Guy Wilson <wilson.guy@gmail.com>
2019-08-27 16:25:55 +0100

    Use mongoose to post TPH data

--- src/webconnect.cpp
+++ src/webconnect.cpp
@@ -3,9 +3,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 
 #include "exception.h"
 #include "currenttime.h"
@@ -63,116 +60,6 @@ static void nullHandler(struct mg_connection * connection, int event, void * p)
 		default:
 			break;
 	}
-}
-
-static void post(const char * pszHost, const int port, const char * pszPath, char * pszBody)
-{
-    struct hostent *	server;
-    struct sockaddr_in 	serv_addr;
-    int					sockfd;
-    int					bytes;
-    int					sent;
-    int					received;
-    int					total;
-    int					message_size;
-    char *				message;
-    char				response[4096];
-    const char *		pszMsgTemplate = "POST %s HTTP/1.0\r\nContent-Type: application/json\r\nContent-Length: %ld\r\n\r\n";
-
-     /* How big is the message? */
-    message_size = 0;
-	message_size += strlen(pszMsgTemplate) + INT_LENGTH;
-	message_size += strlen(pszPath);
-	message_size += strlen(pszBody);
-
-    /* allocate space for the message */
-    message = (char *)malloc(message_size);
-
-    if (message == NULL) {
-    	throw new Exception("ERROR allocating message buffer");
-    }
-
-    /* fill in the parameters */
-	sprintf(message, pszMsgTemplate, pszPath, strlen(pszBody));
-	strcat(message, pszBody);
-
-    /* create the socket */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (sockfd < 0) {
-    	throw new Exception("ERROR opening socket");
-    }
-
-    /* lookup the ip address */
-    server = gethostbyname(pszHost);
-
-    if (server == NULL) {
-    	throw new Exception("ERROR, no such host");
-    }
-
-    /* fill in the structure */
-    memset(&serv_addr, 0, sizeof(serv_addr));
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(port);
-
-    memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
-
-    /* connect the socket */
-    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        throw new Exception("ERROR connecting");
-    }
-
-    /* send the request */
-    total = strlen(message);
-
-    sent = 0;
-
-    do {
-        bytes = write(sockfd, message + sent, total - sent);
-
-        if (bytes < 0) {
-            throw new Exception("ERROR writing message to socket");
-        }
-
-        if (bytes == 0) {
-            break;
-        }
-
-        sent += bytes;
-    }
-    while (sent < total);
-
-    /* receive the response */
-    memset(response, 0, sizeof(response));
-
-    total = sizeof(response) - 1;
-
-    received = 0;
-
-    do {
-        bytes = read(sockfd, response + received, total - received);
-
-        if (bytes < 0) {
-            throw new Exception("ERROR reading response from socket");
-        }
-
-        if (bytes == 0) {
-            break;
-        }
-
-        received += bytes;
-    }
-    while (received < total);
-
-    if (received == total) {
-        throw new Exception("ERROR storing complete response from socket");
-    }
-
-    /* close the socket */
-    close(sockfd);
-
-    free(message);
 }
 
 WebConnector::WebConnector()
@@ -296,11 +183,15 @@ void WebConnector::postTPH(const char * pszPathSuffix, bool save, char * pszTemp
 		pszPressure,
 		pszHumidity);
 
-	strcpy(szWebPath, this->szBasePath);
-	strcat(szWebPath, pszPathSuffix);
+	sprintf(szWebPath, "http://%s:%d%s%s", this->getHost(), this->getPort(), this->szBasePath, pszPathSuffix);
 
 	log.logDebug("Posting to %s [%s]", szWebPath, szBody);
-	post(this->getHost(), this->getPort(), szWebPath, szBody);
+    mg_connect_http(
+                &mgr, 
+                nullHandler, 
+                szWebPath, 
+                "Content-Type: application/json\r\n", 
+                szBody);
 	log.logDebug("Finished post to %s", szWebPath);
 }
 
